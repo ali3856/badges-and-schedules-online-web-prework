fced1e78793c456013ef392cdf6af645edd6d17a
witty-banner-1950 <asimsali95@gmail.com>
2019-05-23 18:46:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,12 @@ def batch_badge_creator (speakers)
   return new_array
 end
 
-def assign_rooms
+def assign_rooms (speakers)
+  new_array = []
+  counter = 1
+  speakers.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return new_array
 end
