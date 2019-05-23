c0680b250b1cb7c578a7858de07a7f47b9c6354b
witty-banner-1950 <asimsali95@gmail.com>
2019-05-23 18:41:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-speakers = ["Edsger", "Ada", "Charles" , "Alan", "Grace", "Linus", "Matz"]
-
 def batch_badge_creator (speakers)
   new_array = []
   speakers.each do |name|
